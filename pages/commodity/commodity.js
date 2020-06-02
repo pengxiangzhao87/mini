e942d64a74cb1539f77bf8585c67220d578da38c
unknown <unknown@example.com>
2020-06-02 23:57:51 +0800

    060203

--- pages/commodity/commodity.js
+++ pages/commodity/commodity.js
@@ -2,6 +2,7 @@
 var app = getApp();
 Page({
   data: {
+    address:"",
     sName:"",    
     commodity:[],
     page:1,
@@ -18,7 +19,18 @@ Page({
       paras.sName = data.sName;
     }
     this.queryCommodity(that,paras);
+    paras.uId=1;
+    paras.isUsed=1;
+    this.queryAddressList(that,paras);
   },
+
+  searchByName:function(e){
+    var sName = e.detail.value; 
+    wx.navigateTo({
+      url: 'search/search?sName='+sName
+    })
+  },
+
   //热销商品分页查询/模糊查询/类别查询
   queryCommodity:function(that,data){
     wx.request({
@@ -30,6 +42,30 @@ Page({
           var list = res.data.data.list;
           that.setData({
             commodity:list
+          })
+        }else{
+          wx.showToast({
+            title: res.data.msg
+          })
+        }
+      },
+      fail(res) {
+        wx.showToast({
+          title: "服务器异常"
+        })
+      }
+    })
+  },
+  queryAddressList:function(that,data){
+    wx.request({
+      url: app.globalData.baseUrl+"user/queryAddressList",
+      method: 'get',
+      data: data,
+      success(res) {
+        if(res.data.code==200){
+          var aCity = res.data.data[0].aCity;
+          that.setData({
+            address:aCity
           })
         }else{
           wx.showToast({
---
+++ pages/commodity/search/search.js
@@ -0,0 +1,10 @@
+// pages/commodity/search/search.js
+Page({
+   
+  data: {
+
+  },
+  onLoad:function(e){
+console.info(e);
+  }
+})
