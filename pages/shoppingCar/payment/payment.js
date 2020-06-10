268456e5d5cdfdead997bc402e0ddd165e1bd441
unknown <unknown@example.com>
2020-06-11 00:33:52 +0800

    0610配送时间

--- pages/shoppingCar/payment/payment.js
+++ pages/shoppingCar/payment/payment.js
@@ -11,7 +11,8 @@ Page({
     allPrice:0,
     //是否下一页返回，1：是
     nextFlag:0,
-
+    today:0,
+    todayHide:false,
     hideFlag: true,//true-隐藏  false-显示
     animationData: {}
   },
@@ -21,9 +22,27 @@ Page({
     var postage = e.postage;
     var totalPrice = e.totalPrice;
     var detailList = JSON.parse(e.json);
-    var startDate = this.getDateRange(new Date());
-    var endDate = new Date(startDate.getTime()+1000*60*30);
-    var dateRange = startDate.getHours()+':'+(startDate.getMinutes()==0?'00':startDate.getMinutes())+'-'+endDate.getHours()+':'+(endDate.getMinutes()==0?'00':endDate.getMinutes());
+    var now = new Date();
+    var month = now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
+    var day = now.getDate() < 10 ? '0' + now.getDate() : now.getDate();
+    var dateRange = '';
+    var todayHide = false;
+    var today=0;
+    var endTime = new Date(now.getFullYear()+'-'+month+'-'+day+' 20:30:00').getTime();
+    if(now.getTime()>endTime){
+      dateRange = '选择时间';
+      todayHide = true;
+      today=1;
+    }else{
+      var startTime = new Date(now.getFullYear()+'-'+month+'-'+day+' 10:15:00');
+      if(now.getTime()<startTime.getTime()){
+        now = startTime;
+      }
+      var startDate = this.getDateRange(now);
+      var endDate = new Date(startDate.getTime()+1000*60*30);
+      dateRange = '今天 '+startDate.getHours()+':'+(startDate.getMinutes()==0?'00':startDate.getMinutes())+' - '+endDate.getHours()+':'+(endDate.getMinutes()==0?'00':endDate.getMinutes());
+    }
+    
     var paras={};
     paras.uId=1;
     paras.isUsed=1;
@@ -40,7 +59,9 @@ Page({
             postage:postage,
             totalPrice:totalPrice,
             allPrice:postage==1?parseFloat(totalPrice)+parseInt(4):totalPrice,
-            address:res.data.data[0]
+            address:res.data.data[0],
+            todayHide:todayHide,
+            today:today
           })
         }else{
           wx.showToast({
@@ -97,12 +118,27 @@ Page({
     }else if(afterMinute>=35 && afterMinute<50){
       endMinute = '45';
     }else if(afterMinute>=50 || afterMinute<5){
-      ++afterHour ;
       endMinute = '00';
     }
     return new Date(afterDate.getFullYear()+'-'+afterDate.getMonth()+'-'+afterDate.getDay()+' '+afterHour+':'+endMinute);
 
   },
+  getRangeList:function(now){
+    var rangeList=[];
+    var startDate = this.getDateRange(now);
+    for(var i=0;i<100;){
+      var endDate = new Date(startDate.getTime()+1000*60*30);
+      var dateRange = startDate.getHours()+':'+(startDate.getMinutes()==0?'00':startDate.getMinutes())+' - '+endDate.getHours()+':'+(endDate.getMinutes()==0?'00':endDate.getMinutes());
+      rangeList[i]=dateRange;
+      if(endDate.getHours()>21){
+        break;
+      }
+      startDate = endDate;
+      ++i;
+    }
+    return rangeList;
+  },
+  //跳转地址
   toAddress:function(){
     wx.navigateTo({
       url: '/pages/address/address'
@@ -114,17 +150,17 @@ Page({
   // 显示遮罩层
   showModal: function () {
     var that = this;
-    var rangeList=[];
-    var startDate = this.getDateRange(new Date());
-    var endDate = new Date(startDate.getTime()+1000*60*30);
-    var dateRange = startDate.getHours()+':'+(startDate.getMinutes()==0?'00':startDate.getMinutes())+'-'+endDate.getHours()+':'+(endDate.getMinutes()==0?'00':endDate.getMinutes());
-
-
-
-
-
+    var now = that.data.todayHide?new Date('2020-06-10 10:15:00'):new Date();
+    var month = now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
+    var day = now.getDate() < 10 ? '0' + now.getDate() : now.getDate();
+    var startTime = new Date(now.getFullYear()+'-'+month+'-'+day+' 10:15:00');
+    if(now.getTime()<startTime.getTime()){
+      now= startTime;
+    }
+    var rangeList=that.getRangeList(now);
     that.setData({
-      hideFlag: false
+      hideFlag: false,
+      rangeList:rangeList
     })
     // 创建动画实例
     var animation = wx.createAnimation({
@@ -140,7 +176,7 @@ Page({
   },
  
   // 隐藏遮罩层
-  hideModal: function () {
+  hideModal:function() {
     var that = this;
     var animation = wx.createAnimation({
       duration: 400,//动画的持续时间 默认400ms
@@ -171,5 +207,36 @@ Page({
     this.setData({
       animationData: this.animation.export(),
     })
+  },
+  today:function(){
+    var that = this;
+    var now = new Date();
+    var month = now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
+    var day = now.getDate() < 10 ? '0' + now.getDate() : now.getDate();
+    var startTime = new Date(now.getFullYear()+'-'+month+'-'+day+' 10:15:00');
+    if(now.getTime()<startTime.getTime()){
+      now= startTime;
+    }
+    var rangeList=that.getRangeList(now);
+    that.setData({
+      rangeList:rangeList,
+      today:0
+    })
+  },
+  tomorrow:function(){
+    var that = this;
+    var rangeList=that.getRangeList(new Date('2020-06-10 10:15:00'));
+    that.setData({
+      rangeList:rangeList,
+      today:1
+    })
+  },
+  checkDate:function(e){
+    var that = this;
+    var date = that.data.today==0?'今天 ':'明天 ';
+    that.setData({
+      dateRange:date+e.currentTarget.dataset.time
+    })
+    that.hideModal();
   }
 })
