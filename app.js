16db79b01eef5d19e921f6af28d8b25d91f272b4
unknown <unknown@example.com>
2020-07-08 01:06:25 +0800

    0707配送时间

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //app.js
 App({
   globalData:{
-    //baseUrl:"http://192.168.1.142:9000/CMTGP/",
-    baseUrl:"http://www.sotardust.cn/CMTGP/",
+    //baseUrl:"https://www.sotardust.cn/CMTGP/",
+    baseUrl:"http://192.168.1.4:9000/CMTGP/",
     searchList:[]
   },
   onLaunch: function () {
+    var that = this;
     // 登录
     wx.login({
       success: res => {
@@ -20,17 +21,49 @@ App({
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              that.globalData.userInfo = res.userInfo
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+                that.userInfoReadyCallback(res)
               }
             }
           })
         }
       }
     })
+    //获取用户唯一ID
+    // wx.login({
+    //   success: function (res) {
+    //     if (res.code) {
+    //       wx.request({
+    //         url: 'https://api.weixin.qq.com/sns/jscode2session',
+    //         data: {
+    //           //填上自己的小程序唯一标识
+    //           appid: 'wx73415b963d21e2f4',
+    //           //填上自己的小程序的 app secret
+    //           secret: '60aa412a233213f7a2924ccf22017148',
+    //           grant_type: 'authorization_code',
+    //           js_code: res.code
+    //         },
+    //         method: 'GET',
+    //         header: { 'content-type': 'application/json' },
+    //         success: function (res) {
+    //           console.info(res)
+    //           wx.setStorage({
+    //             key: 'openid',
+    //             data: res.data.openid,
+    //           })
+
+    //         },
+    //         fail: function (error) {
+    //           console.error("获取用户openId失败");
+    //           console.error(error);
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
   }
 })
--- pages/shoppingCar/payment/payment.js
+++ pages/shoppingCar/payment/payment.js
@@ -11,12 +11,11 @@ Page({
     allPrice:0,
     //是否下一页返回，1：是
     nextFlag:0,
+    rangeList:[],
+    range:[],
     today:0,
-    todayHide:false,
     hideFlag: true,//true-隐藏  false-显示
     animationData: {},
-    method:1,
-    accountPrice:0,
     hidePay:true,
     method:1,
     oId:0,
@@ -28,27 +27,6 @@ Page({
     var postage = e.postage;
     var totalPrice = e.totalPrice;
     var detailList = JSON.parse(e.json);
-    var now = new Date();
-    var month = now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
-    var day = now.getDate() < 10 ? '0' + now.getDate() : now.getDate();
-    var dateRange = '';
-    var todayHide = false;
-    var today=0;
-    var endTime = new Date(now.getFullYear()+'-'+month+'-'+day+' 20:30:00').getTime();
-    if(now.getTime()>endTime){
-      dateRange = '选择时间';
-      todayHide = true;
-      today=1;
-    }else{
-      var startTime = new Date(now.getFullYear()+'-'+month+'-'+day+' 10:15:00');
-      if(now.getTime()<startTime.getTime()){
-        now = startTime;
-      }
-      var prDate = now.getFullYear()+'-'+month+'-'+day;
-      var startDate = this.getDateRange(now,prDate);
-      var endDate = new Date(startDate.getTime()+1000*60*30);
-      dateRange = '今天 '+startDate.getHours()+':'+(startDate.getMinutes()==0?'00':startDate.getMinutes())+' - '+endDate.getHours()+':'+(endDate.getMinutes()==0?'00':endDate.getMinutes());
-    }
     var paras={};
     paras.uId=4;
     paras.isUsed=1;
@@ -59,15 +37,12 @@ Page({
       success(res) {
         if(res.data.code==200){
           that.setData({
-            dateRange:dateRange,
             detailList:detailList,
             baseUrl:baseUrl,
             postage:postage,
             totalPrice:totalPrice,
             allPrice:totalPrice,
-            address:res.data.data[0],
-            todayHide:todayHide,
-            today:today
+            address:res.data.data[0]
           })
         }else{
           wx.showToast({
@@ -82,13 +57,13 @@ Page({
       }
     })
     wx.request({
-      url: baseUrl+"user/queryAccount",
+      url: baseUrl+"order/getRangeTime",
       method: 'get',
       data: paras,
       success(res) {
         if(res.data.code==200){
           that.setData({
-            accountPrice:res.data.data
+            dateRange:res.data.data
           })
         }else{
           wx.showToast({
@@ -134,42 +109,6 @@ Page({
       })
     }
   },
-  getDateRange:function(now,prDate){
-    var time = now.getTime() + 1000*60*15;
-    var afterDate = new Date(time);
-    var afterHour = afterDate.getHours();
-    var afterMinute = afterDate.getMinutes();
-    var endMinute = '00';
-    if(afterMinute>=5 && afterMinute<20){
-      endMinute = '15';
-    }else if(afterMinute>=20 && afterMinute<35){
-      endMinute = '30';
-    }else if(afterMinute>=35 && afterMinute<50){
-      endMinute = '45';
-    }else if(afterMinute>=50 || afterMinute<5){
-      endMinute = '00';
-    }
-    return new Date(prDate+' '+afterHour+':'+endMinute);
-
-  },
-  getRangeList:function(now){
-    var rangeList=[];
-    var month = now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
-    var day = now.getDate() < 10 ? '0' + now.getDate() : now.getDate();
-    var prDate = now.getFullYear()+'-'+month+'-'+day;
-    var startDate = this.getDateRange(now,prDate);
-    for(var i=0;i<100;){
-      var endDate = new Date(startDate.getTime()+1000*60*30);
-      var dateRange = startDate.getHours()+':'+(startDate.getMinutes()==0?'00':startDate.getMinutes())+' - '+endDate.getHours()+':'+(endDate.getMinutes()==0?'00':endDate.getMinutes());
-      rangeList[i]=dateRange;
-      if(endDate.getHours()>21){
-        break;
-      }
-      startDate = endDate;
-      ++i;
-    }
-    return rangeList;
-  },
   //跳转地址
   toAddress:function(){
     wx.navigateTo({
@@ -180,29 +119,43 @@ Page({
   // 显示遮罩层
   showModal: function () {
     var that = this;
-    var now = that.data.todayHide?new Date('2020-06-10 10:15:00'):new Date();
-    var month = now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
-    var day = now.getDate() < 10 ? '0' + now.getDate() : now.getDate();
-    var startTime = new Date(now.getFullYear()+'-'+month+'-'+day+' 10:15:00');
-    if(now.getTime()<startTime.getTime()){
-      now= startTime;
-    }
-    var rangeList=that.getRangeList(now);
-    that.setData({
-      hideFlag: false,
-      rangeList:rangeList
+    var baseUrl = that.data.baseUrl;
+    wx.request({
+      url: baseUrl+"order/getRangeList",
+      method: 'get',
+      data: [],
+      success(res) {
+        if(res.data.code==200){
+          var data = res.data.data;
+          that.setData({
+            rangeList:data,
+            range:data[0].rangeList,
+            hideFlag: false,
+            today:data.length==2?0:1
+          })
+          // 创建动画实例
+          var animation = wx.createAnimation({
+            duration: 400,//动画的持续时间
+            timingFunction: 'ease',//动画的效果 默认值是linear->匀速，ease->动画以低速开始，然后加快，在结束前变慢
+          })
+          that.animation = animation; //将animation变量赋值给当前动画
+          var time1 = setTimeout(function () {
+            that.slideIn();//调用动画--滑入
+            clearTimeout(time1);
+            time1 = null;
+          }, 100)
+        }else{
+          wx.showToast({
+            title: res.data.msg
+          })
+        }
+      },fail(res){
+        wx.showToast({
+          icon:'none',
+          title: '服务器异常'
+        })
+      }
     })
-    // 创建动画实例
-    var animation = wx.createAnimation({
-      duration: 400,//动画的持续时间
-      timingFunction: 'ease',//动画的效果 默认值是linear->匀速，ease->动画以低速开始，然后加快，在结束前变慢
-    })
-    this.animation = animation; //将animation变量赋值给当前动画
-    var time1 = setTimeout(function () {
-      that.slideIn();//调用动画--滑入
-      clearTimeout(time1);
-      time1 = null;
-    }, 100)
   },
  
   // 隐藏遮罩层
@@ -223,47 +176,6 @@ Page({
     }, 220)//先执行下滑动画，再隐藏模块
     
   },
-
-
-  // 显示遮罩层
-  showPayModal: function () {
-    var that = this;
-    that.setData({
-      hidePay: false,
-    })
-    // 创建动画实例
-    var animation = wx.createAnimation({
-      duration: 400,//动画的持续时间
-      timingFunction: 'ease',//动画的效果 默认值是linear->匀速，ease->动画以低速开始，然后加快，在结束前变慢
-    })
-    this.animation = animation; //将animation变量赋值给当前动画
-    var time1 = setTimeout(function () {
-      that.slideIn();//调用动画--滑入
-      clearTimeout(time1);
-      time1 = null;
-    }, 100)
-  },
- 
-  // 隐藏遮罩层
-  hidePayModal:function() {
-    var that = this;
-    var animation = wx.createAnimation({
-      duration: 400,//动画的持续时间 默认400ms
-      timingFunction: 'ease',//动画的效果 默认值是linear
-    })
-    this.animation = animation
-    that.slideDown();//调用动画--滑出
-    var time1 = setTimeout(function () {
-      that.setData({
-        hidePay: true
-      })
-      clearTimeout(time1);
-      time1 = null;
-    }, 220)//先执行下滑动画，再隐藏模块
-    wx.redirectTo({
-      url: '/pages/order/detail/detail?oid='+that.data.oId,
-    })
-  },
   //动画 -- 滑入
   slideIn: function () {
     this.animation.translateY(0).step() // 在y轴偏移，然后用step()完成一个动画
@@ -281,24 +193,19 @@ Page({
   },
   today:function(){
     var that = this;
-    var now = new Date();
-    var month = now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
-    var day = now.getDate() < 10 ? '0' + now.getDate() : now.getDate();
-    var startTime = new Date(now.getFullYear()+'-'+month+'-'+day+' 10:15:00');
-    if(now.getTime()<startTime.getTime()){
-      now= startTime;
-    }
-    var rangeList=that.getRangeList(now);
+    var list = that.data.rangeList;
+    var range = list[0].rangeList;
     that.setData({
-      rangeList:rangeList,
+      range:range,
       today:0
     })
   },
   tomorrow:function(){
     var that = this;
-    var rangeList=that.getRangeList(new Date('2020-06-10 10:15:00'));
+    var list = that.data.rangeList;
+    var range = list[list.length-1].rangeList;
     that.setData({
-      rangeList:rangeList,
+      range:range,
       today:1
     })
   },
@@ -370,7 +277,6 @@ Page({
       }
     }
     data.details = detail;
-    that.showPayModal();
     wx.request({
       url: baseUrl+"order/addOrder",
       method: 'post',
@@ -383,8 +289,6 @@ Page({
           that.setData({
             oId:res.data.data
           })
-          //支付完成，弹窗提示支付完成，跳到待发货列表；未支付，跳到订单详情
-          that.showPayModal();
         }else{
           wx.showToast({
             title: res.data.msg
@@ -396,13 +300,6 @@ Page({
           title: '服务器异常'
         })
       }
-    })
-  },
-  //选择支付方式
-  chechMethod:function(e){
-    var method = e.currentTarget.dataset.method;
-    this.setData({
-      method:method
     })
   },
   //选择配送方式
